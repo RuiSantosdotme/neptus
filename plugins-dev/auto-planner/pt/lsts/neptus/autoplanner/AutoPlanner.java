45545efc46e21d789a03e3d40f35258ef35fd58d
luiscarlosfeliz <ee12155@fe.up.pt>
2016-12-01 19:29:03 +0000

    Plugin Integrado na consola do lado direito
    Por enquanto apenas escreve "bla bla bla" , falta adicionar o botao
    abort e os restantes inputs. Foi necessário alterar o XML da consola
    para ter sucesso. O codigo do autoplanner neste momento está baseado no
    código do PlanEditorState

--- plugins-dev/auto-planner/pt/lsts/neptus/autoplanner/AutoPlanner.java
+++ plugins-dev/auto-planner/pt/lsts/neptus/autoplanner/AutoPlanner.java
@@ -1,72 +1,114 @@
 package pt.lsts.neptus.autoplanner;
-import java.awt.Component;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.JButton;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-/**/
-import java.awt.Dimension;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.text.BadLocationException;
+import java.awt.Color;
+import java.util.Date;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 import com.google.common.eventbus.Subscribe;
 
 import net.miginfocom.swing.MigLayout;
-import pt.lsts.imc.Abort;
-import pt.lsts.imc.AcousticOperation;
-import pt.lsts.imc.AcousticOperation.OP;
-import pt.lsts.imc.IMCMessage;
-import pt.lsts.imc.TextMessage;
-import pt.lsts.neptus.comm.manager.imc.ImcMsgManager;
-import pt.lsts.neptus.comm.manager.imc.ImcSystem;
-import pt.lsts.neptus.comm.manager.imc.ImcSystemsHolder;
-import pt.lsts.neptus.comm.manager.imc.MessageDeliveryListener;
-import pt.lsts.neptus.console.notifications.Notification;
-import pt.lsts.neptus.i18n.I18n;
-import pt.lsts.neptus.plugins.NeptusProperty;
-import pt.lsts.neptus.types.vehicle.VehicleType.SystemTypeEnum;
-import pt.lsts.neptus.util.GuiUtils;
-import pt.lsts.neptus.util.conf.GeneralPreferences;
-/**/
-
+import pt.lsts.imc.IMCDefinition;
+import pt.lsts.imc.PlanControlState;
+import pt.lsts.imc.PlanDB;
+import pt.lsts.imc.PlanDB.OP;
+import pt.lsts.imc.PlanDB.TYPE;
+import pt.lsts.neptus.NeptusLog;
 import pt.lsts.neptus.console.ConsoleLayout;
 import pt.lsts.neptus.console.ConsolePanel;
+import pt.lsts.neptus.console.events.ConsoleEventMainSystemChange;
+import pt.lsts.neptus.i18n.I18n;
+import pt.lsts.neptus.plugins.NeptusProperty;
+import pt.lsts.neptus.plugins.NeptusProperty.LEVEL;
 import pt.lsts.neptus.plugins.PluginDescription;
-import pt.lsts.neptus.plugins.Popup;
-import pt.lsts.neptus.plugins.Popup.POSITION;
-import pt.lsts.neptus.plugins.map.MapEditor;
+import pt.lsts.neptus.plugins.update.Periodic;
+import pt.lsts.neptus.util.DateTimeUtil;
+import pt.lsts.neptus.util.GuiUtils;
 
 /**
  * @author Equipa C - SEAI 2016
  *
  */
 @PluginDescription(name = "Auto Planner")
-@Popup(accelerator='Y',pos = POSITION.RIGHT, height=200, width = 200)
-@SuppressWarnings("serial")
+//@Popup(accelerator='Y',pos = POSITION.RIGHT, height=200, width = 200)
+//@SuppressWarnings("serial")
 
 public class AutoPlanner extends ConsolePanel {
 
     /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    /**
      * @param console
      */
+    
+    private JLabel stateValueLabel;
+    private JLabel stateLabel;
+    private JLabel planIdValueLabel;
+    private JLabel planIdLabel;
+    private JLabel nodeIdValueLabel;
+    private JLabel nodeIdLabel;
+    private JLabel outcomeTitleLabel;
+    private JLabel outcomeLabel;
+    
+    
+    private PlanControlState.STATE state;
+    private String planId = "";
+    private String nodeId = "";
+    private String lastOutcome = "<html><font color='0x666666'>" + I18n.text("N/A") + "</font>";
+    private int nodeTypeImcId = -1;
+    private long nodeStarTimeMillisUTC = -1;
+    private long nodeEtaSec = -1;
+    private long lastUpdated = -1;
+    
+    @NeptusProperty(name = "Request plans automatically", userLevel=LEVEL.ADVANCED, category="Planning", description = "Select if Neptus should ask the vehicle for plans it is executing but Neptus doesn't know about")
+    public boolean requestPlans = false;
+    
     public AutoPlanner(ConsoleLayout console) {
         super(console);
+        removeAll();
+        initialize();
+    }
+    
+    
+    private void initialize() {
+        setSize(200, 200);
+        this.setLayout(new MigLayout("ins 0"));
+        stateValueLabel = new JLabel();
+        stateValueLabel.setText("");
+        stateLabel = new JLabel();
+        stateLabel.setText("<html><b>" + I18n.text("BLABLABLABLABLABLABLA") + ": ");
+
+        this.add(stateLabel, "");
+        this.add(stateValueLabel, "wrap");
+        
+
+        nodeIdValueLabel = new JLabel();
+        nodeIdValueLabel.setText("");
+        nodeIdLabel = new JLabel();
+        // / This is a plan node, keep it in one word.
+        nodeIdLabel.setText("<html><b>" + I18n.textc("Man.", "Maneuver") + ": ");
+
+        this.add(nodeIdLabel);
+        this.add(nodeIdValueLabel, "wrap");
+
+        outcomeTitleLabel = new JLabel("<html><b>" + I18n.text("Outcome") + ": ");
+        outcomeTitleLabel.setHorizontalAlignment(SwingConstants.LEADING);
+
+        outcomeLabel = new JLabel(lastOutcome);
+        this.add(outcomeTitleLabel);
+        this.add(outcomeLabel, "wrap");
+
+      
     }
 
     @Override
     public void initSubPanel() {
-        removeAll();
+        /*removeAll();
 
         Action sendAbortAction = new AbstractAction(I18n.text("Send Abort")) {
 
@@ -88,7 +130,7 @@ public class AutoPlanner extends ConsolePanel {
 
         JButton sendAbort = new JButton(sendAbortAction);
 
-        add(sendAbort);
+        add(sendAbort);*/
     }
 
     @Override
